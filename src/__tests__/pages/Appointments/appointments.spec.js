32ab37116ee675f7646afd6b36ec74fa87fbc558
Gabriel Monteiro <gabrielqmonteiro@gmail.com>
2021-04-23 19:28:43 -0300

    Added some snapshot tests

--- src/__tests__/pages/Appointments/appointments.spec.js
+++ src/__tests__/pages/Appointments/appointments.spec.js
@@ -4,23 +4,25 @@ import { render, screen } from '@testing-library/react';
 import AppointmentList from '../../../pages/Appointments';
 
 describe('AppointmentList Page', () => {
+  it('must render', () => {
+    const { asFragment } = render(<AppointmentList />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
   it('Must contains a filter button', () => {
     render(<AppointmentList />);
-
     const filterButton = screen.getByRole('button', {
       name: 'Filtrar',
     });
-
     expect(filterButton).toBeInTheDocument();
   });
 
   it('Must contains a remove filter button', () => {
     render(<AppointmentList />);
-
     const removeFilterButton = screen.getByRole('button', {
       name: 'Remover Filtro',
     });
-
     expect(removeFilterButton).toBeInTheDocument();
   });
 });
